0183328c5800bc32eba488b6a9bccf52d9f7cd9f
Peter Simon <psi@estos.de>
2018-01-07 13:26:59 +0100

    - persistence module, open file. uploadToKRT2(), GUI with img/map

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # KRT2Mngr
 Dittel/TQ-Aviation radio transceiver (KRT2) RemoteControl
+
+## History
+- [X] persistence module, open file. uploadToKRT2(), GUI with img/map
---
+++ persist.js
@@ -0,0 +1,27 @@
+﻿var PersistModul = PersistModul || (function () {
+	return {
+		rgStations: [
+			{ nFrequence: 123500, sName: "Geratshf" },
+			{ nFrequence: 123995, sName: "BadWoer" },
+			{ nFrequence: 120100, sName: "KemptenD" },
+			{ nFrequence: 126025, sName: "Agathazl" },
+			{ nFrequence: 118500, sName: "Memmingn" },
+			{ nFrequence: 122500, sName: "Paterzel" },
+			{ nFrequence: 126900, sName: "PETER" }
+		],
+
+		readSingleFile: function (e) {
+			var file = e.target.files[0];
+			if (!file) return;
+
+			var reader = new FileReader();
+			reader.onload = function (e) {
+				var contents = e.target.result;
+				// displayContents(contents);
+				console.log(e.target.result);
+				var rgStations = str.split("\n\a");
+			};
+			reader.readAsText(file);
+		}
+	};
+})();
